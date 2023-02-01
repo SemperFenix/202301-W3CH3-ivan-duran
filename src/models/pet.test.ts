660476ebb8bbae325f0e9b5da998a452822a6e87
Ivan Duran <dureivan@gmail.com>
2023-02-01 19:13:26 +0100

    Update pet test

--- src/models/pet.test.ts
+++ src/models/pet.test.ts
@@ -1,14 +1,24 @@
 import { Pet } from './pet';
 
 describe('Given Pet class', () => {
-  describe('When called', () => {
+  describe('When called without owner parameter', () => {
     test('Then it should return an object with the parameters given', () => {
       const pet = new Pet('Joti', 'Gato mein khun');
       expect(pet.name).toBe('Joti');
-      expect(typeof pet.id).toBe(Number);
+      expect(typeof pet.id).toBe('string');
       expect(pet.specie).toBe('Gato mein khun');
       expect(pet.owner).toBe('Sin dueño');
-      expect(pet.isAdopted).toBe('False');
+      expect(pet.isAdopted).toBe(false);
+    });
+  });
+  describe('When called with an owner parameter', () => {
+    test('Then it should return an object with the parameters given', () => {
+      const pet = new Pet('Joti', 'Gato mein khun', 'Amaya');
+      expect(pet.name).toBe('Joti');
+      expect(typeof pet.id).toBe('string');
+      expect(pet.specie).toBe('Gato mein khun');
+      expect(pet.owner).toBe('Amaya');
+      expect(pet.isAdopted).toBe(false);
     });
   });
 });
