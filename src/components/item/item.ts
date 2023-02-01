c6880e96c179f1dad3ee2d797a87c181851364f1
SemperFenix <dureivan@gmail.com>
2023-02-02 00:15:58 +0100

    Add item component

--- src/components/item/item.ts
+++ src/components/item/item.ts
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-vars */
 import { Pet } from '../../models/pet';
 import { Component } from '../component/component';
-
+import './item.scss';
 export class Item extends Component {
+  template: string;
   constructor(public selector: string, public pet: Pet) {
     super();
     this.template = this.createTemplate();
@@ -11,10 +12,11 @@ export class Item extends Component {
 
   createTemplate() {
     return `<li>
-          <img src="assets/img/${this.pet.name}.jpg" width="300px">
+          <img src="assets/img/${this.pet.name}.jpg" width="250px">
           <div><span class = "bold">Nombre:</span> ${this.pet.name}.</div>
           <div><span class = "bold">Raza:</span> ${this.pet.specie}.</div>
           <div><span class = "bold">Dueño:</span> ${this.pet.owner}.</div>
+          <input type=button value="🗑" class="delete">
           </li>`;
   }
 }
