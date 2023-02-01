4d497bf80530ff139a048df4c126db07589a2ede
Ivan Duran <dureivan@gmail.com>
2023-02-01 21:34:33 +0100

    Modify index html and index ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 /* eslint-disable no-new */
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
+import { PetListCreator } from './components/list/list';
+import { mockPets } from './mock/pets';
 
 new Header('.root');
+new PetListCreator('.pet-list', mockPets);
 new Footer('.root');
