f8e98e9c62efe3a92d675729277adddca52e84b9
Ivan Duran <dureivan@gmail.com>
2023-02-01 20:05:05 +0100

    Update component.ts

--- src/components/component/component.ts
+++ src/components/component/component.ts
@@ -2,8 +2,8 @@ export abstract class Component {
   selector!: string;
   template!: string;
 
-  render() {
+  render(place: globalThis.InsertPosition) {
     const element = document.querySelector(this.selector) as HTMLElement;
-    element.insertAdjacentHTML('afterbegin', this.template);
+    element.insertAdjacentHTML(place, this.template);
   }
 }
