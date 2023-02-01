b5c185d698d66d46c3ee65c2457181e31e294654
SemperFenix <dureivan@gmail.com>
2023-02-02 00:16:35 +0100

    Update index.ts with new list and item component

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 /* eslint-disable no-new */
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
+import { Item } from './components/item/item';
 import { PetListCreator } from './components/list/list';
 import { mockPets } from './mock/pets';
-
+import './index.scss';
 new Header('.root');
-new PetListCreator('.pet-list', mockPets);
 new Footer('.root');
+new PetListCreator('.pet-list', mockPets);
+// New Item('.root', mockPets[0]);
