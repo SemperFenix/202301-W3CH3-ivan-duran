f23a39db3b20c6b5ff3375ae98aee6cfba7b0549
Ivan Duran <dureivan@gmail.com>
2023-02-01 20:04:29 +0100

    Add footer and footer.scss

---
+++ src/components/footer/footer.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { Component } from '../component/component';
+import './footer.scss';
+
+export class Footer extends Component {
+  constructor(public selector: string) {
+    super();
+    this.template = this.createTemplate();
+    this.render('beforeend');
+  }
+
+  private createTemplate() {
+    // La hacemos de uso interno con la etiqueta private
+    return `
+    <footer class ="footer">
+      <adress>Contacta con nosotros en el 555-965-874</adress>
+    </footer>`;
+  }
+}
