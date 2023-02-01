89ae564eacc99a91c9bc2edebf59d2edb48a56fd
SemperFenix <dureivan@gmail.com>
2023-02-02 00:56:18 +0100

    Update component add

--- src/components/add/add.ts
+++ src/components/add/add.ts
@@ -2,7 +2,7 @@
 import { Pet } from '../../models/pet';
 import { Component } from '../component/component';
 import { Item } from '../item/item';
-
+import './add.scss';
 export class Add extends Component {
   template: string;
   constructor(public selector: string) {
@@ -14,6 +14,7 @@ export class Add extends Component {
 
   createTemplate() {
     return `<form action="">
+              <p>Añade tu mascota</p>
               <ul>
                 <li><label for="Name">Nombre<input type="text" id="Name"></label></li>
                 <li><label for="Specie">Especie<input type="text" id="Specie"></label></li>
@@ -38,6 +39,19 @@ export class Add extends Component {
       petName!.value = '';
       petSpecie!.value = '';
       petOwner!.value = '';
+    });
+
+    petOwner?.addEventListener('keydown', (event) => {
+      if (event.keyCode == 13) {
+        // eslint-disable-next-line no-new
+        new Item(
+          '.pets-list',
+          new Pet(petName?.value, petSpecie?.value, petOwner?.value)
+        );
+        petName!.value = '';
+        petSpecie!.value = '';
+        petOwner!.value = '';
+      }
     });
   }
 }
